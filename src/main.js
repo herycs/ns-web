f61a1be7238b827c7fa5654ebe2b1b3ff92a37fb
gmch1 <1731216262@qq.com>
2021-04-06 00:09:16 +0800

    nothing-link-vant

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
+import { Button, Cell, CellGroup, Popup } from "vant";
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
 Vue.config.productionTip = false;
-
+Vue.use(Button);
+Vue.use(Cell);
+Vue.use(CellGroup);
+Vue.use(Popup);
 new Vue({
   render: h => h(App),
   router,
--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,36 +1,44 @@
 <template>
   <div id="detail">
-    <detail-nav-bar/>
-    <h1>id为： {{iid}}  </h1>
+    <detail-nav-bar />
+    <!-- <h1>id为： {{iid}}  </h1> -->
+    <van-popup v-model="show">
+      评论区内容
+    </van-popup>
   </div>
 </template>
 
 <script>
-  import DetailNavBar from './childCom/DetailNavBar'
+import DetailNavBar from "./childCom/DetailNavBar";
+import { getDetail } from "network/detail";
 
-  import {getDetail} from "network/detail";
-
-  export default {
-    name: 'Detail',
-    components: {
-      DetailNavBar
-    },
-    data() {
-      return {
-        iid: null
-      }
-    },
-    created() {
-      // console.log(this.$route.params);
-      this.iid = this.$route.params.id
-      // 1.获取传入的id
-      // 2.发起网络请求
-      // getDetail(this.iid).then(res => {
-      //   console.log(res);
-      // })
+export default {
+  name: "Detail",
+  components: {
+    DetailNavBar
+  },
+  data() {
+    return {
+      iid: null,
+      show: false
+    };
+  },
+  methods: {
+    showPopup() {
+      this.show = true;
     }
+  },
+  mounted() {
+    // console.log(this.$route.params);
+    this.iid = this.$route.params.id;
+    // 1.获取传入的id
+    // 2.发起网络请求
+    // getDetail(this.iid).then(res => {
+    //   console.log(res);
+    // })
+    this.showPopup();
   }
+};
 </script>
 
-<style>
-</style>
+<style></style>
